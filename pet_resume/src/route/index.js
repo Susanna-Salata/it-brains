5918a6337b35efa91e5b82a777c26ae9460dd6cc
Susanna-Salata <susanna.kiev@gmail.com>
2023-04-08 12:01:34 +0200

    pet_hw_1

--- pet_resume/src/route/index.js
+++ pet_resume/src/route/index.js
@@ -23,15 +23,40 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-    title:'Susanna Salata | Resume',
-
+    title: 'Susanna Salata | Resume',
+    
     name: {
       firstname:'Susanna',
       lastname: 'Salata',
     },
 
-    position: 'Junior Fullstack JS Developer',
+    position: 'Junior Fullstack JS Developer', 
+   
+    salary: '600$',
 
+    summary: {
+      title: 'Summary',
+      text: `Open-minded for new technologies, with 1 years of experience in development. 
+            Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. 
+            Good team player, every colleague is a friend to me.`,
+    },
+
+    experience: {
+      job_1: {
+        title: 'Experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics 
+            (tournament position, goals etc), analyzing by simple mathematics models and preparing probability 
+            for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+      
+      job_2: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics 
+            (tournament position, goals etc), analyzing by simple mathematics models and preparing probability 
+            for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+        
     social: {
       email: {
         text: 'susanna.kiev@gmail.com',
